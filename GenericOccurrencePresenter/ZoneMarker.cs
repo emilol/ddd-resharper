26d7dea5cd9236b59a1eb0a0f993bd16d41d4f74
Emily Poole <emilypoole@live.com.au>
2015-11-24 07:22:34 +1000

    Add Zone Marker

---
+++ GenericOccurrencePresenter/ZoneMarker.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services.Navigation;
+
+namespace GenericOccurrencePresenter
+{
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<NavigationZone>
+    {
+         
+    }
+}
